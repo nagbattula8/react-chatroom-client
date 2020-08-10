667c9a3031772b2b7abc102a2ae060a564c497f2
nagbattula8 <nagbattula8@gmail.com>
2020-08-09 23:43:07 -0700

    Updates

---
+++ precache-manifest.245b73507b517882d8d7eae9979137e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66fbf91a1b7bd21c802a70510a35459f",
+    "url": "/react-chatroom-client/index.html"
+  },
+  {
+    "revision": "77a4f5d0be11c2982e46",
+    "url": "/react-chatroom-client/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "ca368685f8d4567ce560",
+    "url": "/react-chatroom-client/static/js/2.9d31e6ba.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/react-chatroom-client/static/js/2.9d31e6ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a4f5d0be11c2982e46",
+    "url": "/react-chatroom-client/static/js/main.a8e02c01.chunk.js"
+  },
+  {
+    "revision": "9515cf2b1683ba79ead4",
+    "url": "/react-chatroom-client/static/js/runtime-main.ef52394e.js"
+  }
+]);
